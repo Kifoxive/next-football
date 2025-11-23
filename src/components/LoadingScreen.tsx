f68cc33d53018412797231bf3ecc6d5b7113ed88
Kifoxive <palianycia@seznam.cz>
2025-11-23 22:00:44 +0100

    🚧 added loading screen

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Box, Typography } from "@mui/material";
+import Logo from "./icons";
+import { useTheme } from "next-themes";
+
+export default function LoadingScreen() {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <Box className="flex w-screen h-screen items-center justify-center">
+      <Box className="flex gap-2 items-center animate-bounce">
+        <Logo light={resolvedTheme === "dark"} transparentBg />
+        <Typography variant="subtitle1" fontWeight="bold">
+          Next Football
+        </Typography>
+      </Box>
+      <Box className="absolute bottom-2 right-3 opacity-50">
+        <Typography variant="subtitle2" fontWeight="thin">
+          v0.1.0
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
